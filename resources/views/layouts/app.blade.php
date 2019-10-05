5fa15ba7d5def39238a628106d38f8727bce3f6a
gacek <szymon.gackowski@gmail.com>
2019-10-05 18:10:10 +0200

    Added vue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,11 +48,12 @@
     <link href="{{ asset('assets') }}/css/now-ui-dashboard.css?v=1.3.0" rel="stylesheet"/>
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link href="{{ asset('assets') }}/demo/demo.css" rel="stylesheet"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 
 </head>
 
 <body class="{{ $class ?? '' }}">
-<div class="wrapper">
+<div class="wrapper" id="app">
     @if(Request::is('/'))
         @include('layouts.page_template.guest')
     @else
@@ -79,6 +80,7 @@
 <script src="{{ asset('assets') }}/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
 <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
 <script src="{{ asset('assets') }}/demo/demo.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 @stack('js')
 </body>
 
